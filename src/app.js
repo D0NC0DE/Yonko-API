28e7da8d8db0caf10652d54ff845f8dce71842b4
Winner Olusola <OLUSOLAWINNER1@GMAIL.COM>
2024-10-18 21:32:17 +0100

    ➕ ~ route handler

--- src/app.js
+++ src/app.js
@@ -12,11 +12,19 @@ const baseRouter = require('./routes/baseRouter');
 const app = express();
 
 app.use(corsMiddleware); // CORS handling
-
 app.use(bodyParser.json()); // application/json
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Yonko API');
+});
 
 // Routes
 app.use('/api/v1', baseRouter);
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
 app.use(handleError);
 
